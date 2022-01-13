a73b46d0b31a67377dbc116f0a5210ce15e09686
Patrick Dolan <dolanp1992@gmail.com>
2022-01-12 20:44:05 -0800

    fix getMonsters function
    Co-authored-by: Wajma Niazi <w1niazi@hotmail.com>
    Co-authored-by: Amber Wilwand <amoothenielson@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import DnD5eService from "./js/DnD5eService";
 
 
 function getMonsters(response) {
-  if (response.main) {
-    $('.showcharacters').text(`The strengths are ${response.challenge_rating}`);
+  if (response.results) {
+    $('.showcharacters').text(`The name of the monster is ${response.results[0]["name"]} and its armor class is ${response.results[0]["armor_class"]} `);
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
   }
 }
 $(document).ready(function() {
-  $('#formOne').click(function() {
+  $('#character_btn').click(function() {
     let ratings = $('#challenge_rating').val();
     DnD5eService.getMonsters(ratings)
       .then(function(response) {
